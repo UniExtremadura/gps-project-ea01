16f1c6f95bfff9b7c76ead102d56c717b49f9c70
danielAbad1 <114291002+danielAbad1@users.noreply.github.com>
2023-11-19 20:22:13 +0100

    ImageFragment has been modified to save a user's like in the database

--- app/src/main/java/es/unex/giiis/asee/snapmap_ea01/view/home/ImageFragment.kt
+++ app/src/main/java/es/unex/giiis/asee/snapmap_ea01/view/home/ImageFragment.kt
@@ -10,17 +10,21 @@ import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import es.unex.giiis.asee.snapmap_ea01.R
 import es.unex.giiis.asee.snapmap_ea01.data.model.Photo
+import es.unex.giiis.asee.snapmap_ea01.data.model.User
+import es.unex.giiis.asee.snapmap_ea01.data.model.UserPhotoLikeRef
 import es.unex.giiis.asee.snapmap_ea01.database.SnapMapDatabase
 import es.unex.giiis.asee.snapmap_ea01.databinding.FragmentImageBinding
 import kotlinx.coroutines.launch
+
+private const val ARG_PHOTO_ID = "photoId"
 
 class ImageFragment : Fragment() {
 
     private lateinit var binding: FragmentImageBinding
     private lateinit var db: SnapMapDatabase
     private lateinit var photo: Photo
-
-
+    private lateinit var currentUser: User
+    private var currentPhotoId: Long = -1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,64 +34,54 @@ class ImageFragment : Fragment() {
         binding = FragmentImageBinding.inflate(inflater, container, false)
 
         db = SnapMapDatabase.getInstance(requireContext())!!
-        /*
-        val view = inflater.inflate(R.layout.fragment_image, container, false)
 
-        // Obtenemos la referencia de las ImageView ivLike e ivComment
-        val likeImageView: ImageView = view.findViewById(R.id.ivLike)
-        val commentImageView: ImageView = view.findViewById(R.id.ivComment)
+        // Obtiene el objeto User de la actividad anterior
+        currentUser =
+            requireActivity().intent.getSerializableExtra(HomeActivity.USER_INFO) as User
 
-        // Agregamos OnClickListener a la ImageView ivLike
-        likeImageView.setOnClickListener {
-            val newColor = ContextCompat.getColor(requireContext(), R.color.like)
-            likeImageView.setColorFilter(newColor)
-        }
-
-        // Agregamos OnClickListener a la ImageView ivComment
-        commentImageView.setOnClickListener {
-            // Navegamos a CommentsFragment cuando se hace clic en ivComment
-            val commentsFragment = CommentsFragment()
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, commentsFragment)
-                .addToBackStack(null)
-                .commit()
-        }
-        */
         setUpUI()
         setUpListeners()
         return binding.root
     }
 
     private fun setUpUI() {
-        with(binding){
+        with(binding) {
             val args = ImageFragmentArgs.fromBundle(requireArguments())
-            val photoId = args.photoId
+            currentPhotoId = args.photoId
 
             lifecycleScope.launch {
-
-                photo = db.photoDao().getPhoto(photoId)
+                photo = db.photoDao().getPhoto(currentPhotoId)
 
                 Glide.with(requireContext())
                     .load(photo.photoURL)
                     .placeholder(R.drawable.baseline_access_time_24)
                     .into(ivImage)
 
-                tvAuthor.text = "from ${db.userDao().getUserById(photo.owner!!)}"
+                // Utiliza el ID del usuario del objeto User
+                tvAuthor.text = "from ${db.userDao().getUserById(currentUser.userId)}"
             }
         }
     }
 
     private fun setUpListeners() {
-        with(binding){
-            val args = ImageFragmentArgs.fromBundle(requireArguments())
-            val photoId = args.photoId
-
+        with(binding) {
             ivLike.setOnClickListener {
+                // Cambiar el color del botón like
                 val newColor = ContextCompat.getColor(requireContext(), R.color.like)
                 ivLike.setColorFilter(newColor)
+
+                // Registrar el like en la base de datos
+                val userPhotoLikeRef =
+                    currentUser.userId?.let { it1 -> UserPhotoLikeRef(it1, currentPhotoId) }
+                lifecycleScope.launch {
+                    if (userPhotoLikeRef != null) {
+                        db.userPhotoLikeRefDao().insertUserPhotoLikeRef(userPhotoLikeRef)
+                    }
+                }
             }
+
             ivComment.setOnClickListener {
-                val commentsFragment = CommentsFragment.newInstance(photoId)
+                val commentsFragment = CommentsFragment.newInstance(currentPhotoId)
                 requireActivity().supportFragmentManager.beginTransaction()
                     .replace(R.id.nav_host_fragment, commentsFragment)
                     .addToBackStack(null)
@@ -97,8 +91,12 @@ class ImageFragment : Fragment() {
     }
 
     companion object {
-        fun newInstance(): ImageFragment {
-            return ImageFragment()
+        fun newInstance(photoId: Long): ImageFragment {
+            return ImageFragment().apply {
+                arguments = Bundle().apply {
+                    putLong(ARG_PHOTO_ID, photoId)
+                }
+            }
         }
     }
 }
